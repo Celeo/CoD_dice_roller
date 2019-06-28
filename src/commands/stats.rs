7654a356a2b4db3810e872a1bb95c3343dfa45b8
Celeo <celeodor@gmail.com>
2019-06-27 18:25:23 -0700

    Bulk-editing stats

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -24,7 +24,6 @@ pub fn stats(context: &mut Context, message: &Message, args: Args) -> CommandRes
             .push_codeblock(&character, None)
             .build();
         message.channel_id.say(&context.http, &response)?;
-        return Ok(());
     } else if first_arg == "edit" {
         if args.len() != 3 {
             message
@@ -32,8 +31,8 @@ pub fn stats(context: &mut Context, message: &Message, args: Args) -> CommandRes
                 .say(&context.http, "`!stats edit <stat_name> <stat_value>`")?;
             return Ok(());
         }
-        let stat_key = args.single::<String>().unwrap();
-        let stat_value = match args.single::<i64>() {
+        let stat_key = args.trimmed().single::<String>().unwrap();
+        let stat_value = match args.trimmed().single::<i64>() {
             Ok(i) => i,
             Err(_) => {
                 message.channel_id.say(
@@ -47,6 +46,33 @@ pub fn stats(context: &mut Context, message: &Message, args: Args) -> CommandRes
         character.set_value(&stat_key, stat_value);
         cs.save(&LOAD_PATH)?;
         message.channel_id.say(&context.http, "Got it.")?;
+    } else if first_arg == "bulk" {
+        let mut change_counter = 0u64;
+        while let Ok(next) = args.trimmed().single::<String>() {
+            let parts: Vec<&str> = next.split('=').collect();
+            if parts.len() != 2 {
+                message.channel_id.say(&context.http, "Bulk-editing stats is done in the form: name1=value1 name2=value2 name3=value3 ...")?;
+                return Ok(());
+            }
+            let stat_value = match parts[1].parse::<i64>() {
+                Ok(v) => v,
+                Err(_) => {
+                    message.channel_id.say(
+                        &context.http,
+                        "Bulk-editing stats values must be whole numbers, i.e. 1, 49, 0",
+                    )?;
+                    return Ok(());
+                }
+            };
+            character.set_value(parts[0], stat_value);
+            change_counter += 1;
+        }
+        cs.save(&LOAD_PATH)?;
+        if change_counter > 0 {
+            message
+                .channel_id
+                .say(&context.http, &format!("Saved {} stats", change_counter))?;
+        }
     }
     Ok(())
 }
